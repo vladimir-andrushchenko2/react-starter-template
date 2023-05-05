1c0319a299db4441d61f2e414acb6b4ba8792e33
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2023-05-05 22:45:47 +0300

    starter files

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import App from './App'
+
+const root = createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
